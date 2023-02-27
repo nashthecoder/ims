653905fbd07e6e3369ab0330c16c627102d15291
nashthecoder <naijeria@gmail.com>
2023-02-27 23:13:07 +0300

    added upload page and upload model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :uploads
   devise_for :users
-  root 'home#index'
-  
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy' 
+    root 'home#index'
+    end
   get 'home/index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
